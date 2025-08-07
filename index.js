6062f712e79f6007ca3a91f7cf63c4488ac3316e
YasserSoltan <161496480+YasserSoltan@users.noreply.github.com>
2025-08-07 13:50:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,18 @@
 // server.use(router);
 
 const jsonServer = require("json-server"); // importing json-server library
+const auth = require('json-server-auth'); // Import auth middleware
+
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+server.db = router.db;
+
 
 server.use(middlewares);
+server.use(auth);  // <-- Auth middleware BEFORE router
 server.use(router);
 
 server.listen(port);
