885ce0bff6bcd1543c40dd6031284d0e38d3287c
YasserSoltan <yassersoltan64@gmail.com>
2025-08-07 13:04:52 +0300

    fix json server auth

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const jsonServerAuth = require("json-server-auth");
-const server = jsonServerAuth.create();
-const router = jsonServerAuth.router("db.json");
-const middlewares = jsonServerAuth.defaults();
-const port = process.env.PORT || 3000;
+// const jsonServerAuth = require("json-server-auth");
+// const server = jsonServerAuth.create();
+// const router = jsonServerAuth.router("db.json");
+// const middlewares = jsonServerAuth.defaults();
+// const port = process.env.PORT || 3000;
 
-server.use(middlewares);
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
+
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+server.db = router.db; // Required for auth
+server.use(auth);      // Attach auth middleware
 server.use(router);
-
-server.listen(port);
